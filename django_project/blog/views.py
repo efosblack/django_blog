a5985c90234709b896c4869ca0d74c187b033d32
efosblack <eriamiatoeefe11@gmail.com>
2022-06-14 08:26:26 -0400

    added templates and static files

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+
+    {
+        'author': 'eriamiatoe',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2120'
+    },
+        {
+        'author': 'jon snow',
+        'title': 'Blog post 2',
+        'content': 'second post content',
+        'date_posted': 'August 28, 2121'
+    }
+]
+
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
-
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
+
+
+
