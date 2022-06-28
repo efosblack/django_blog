d1d49d1ab63f493e167f8f4beb9e2bd8b952263e
efosblack <eriamiatoeefe11@gmail.com>
2022-06-28 08:18:47 -0400

    create,update and delete post feature added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin as LR, UserPassesTestMixin as UP
+from django.views.generic import (
+    ListView,
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
 
@@ -11,8 +19,51 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LR, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LR, UP, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LR, UP, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title' : 'About'})
 
 
 
